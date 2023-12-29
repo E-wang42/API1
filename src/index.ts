63878308dfc7d7339a1640223905158e2b4ee677
eric <ericwang42@outlook.com>
2023-12-28 20:19:22 -0800

    fix DB connect

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
 import router from "./routes";
+import morgan from "morgan";
 
 const app = express();
 
@@ -18,24 +19,30 @@ app.use(
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(morgan("dev"));
 
 const PORT: number | string = process.env.PORT || 8080;
 const server = http.createServer(app);
-const MONGO_URL = "mongodb://localhost:27017/userAuth";
+const MONGO_URL = "mongodb://127.0.0.1:27017/api1";
 
 mongoose.Promise = Promise;
-try {
-  async function connectDB() {
-    await mongoose.connect(MONGO_URL, {});
-    console.log("MongoDB connected");
-  }
-  connectDB();
-} catch (err) {
-  console.error(err);
-}
-mongoose.connection.on("eRRor", (error: Error) => console.log(error));
-app.use("/", router());
 
+async function connectDB() {
+  try {
+    await mongoose.connect(MONGO_URL);
+  } catch (err) {
+    console.error(err);
+  }
+}
+connectDB();
+
+mongoose.connection.on("connected", () => console.log("--DB Connected--"));
+mongoose.connection.on("error", (err: Error) =>
+  console.error("DB connection error:", err)
+);
+mongoose.connection.on("disconnected", () => console.log("DB disconnected"));
 server.listen(PORT, (): void => {
   console.log(`***SERVER RUNNING ON PORT ${PORT}***`);
 });
+
+app.use("/", router());
